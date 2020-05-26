32139fca806885fe878152223078b70bafa06707
charlesmartinreed <charlesmartinreed@icloud.com>
2020-05-26 15:49:19 -0500

    Hello world basics.

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// Function, named 'main', that returns a integer value.
+int main() {
+//    anything between the {} will be run when the main() is invoked by our function. This is called a CODE BLOCK. Each line of syntax is an instruction and these are completed from top to bottom.
+
+    std::cout << "Hello, World!" << std::endl; // cout = 'console out'
+    std::cout << "My name is Charles." << std::endl; // endl = 'end line'
+
+    std::cout << "   /|" << std::endl;
+    std::cout << "  / |" << std::endl;
+    std::cout << " /  |" << std::endl;
+    std::cout << "/_ _|" << std::endl;
+
+//    this terminate the main() function
+    return 0;
+}
